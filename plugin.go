6e8d3d738549ed456e0272f7830725f76106cdb2
Devansh Mathur <devanshmathur@harness.io>
2024-10-23 17:27:20 +0530

    Updating secret using drone-plugin-lib

--- plugin.go
+++ plugin.go
@@ -76,14 +76,14 @@ func (p Plugin) Exec() error {
 		fmt.Println("Secret set successfully.")
 	}
 
-	// // Example for testing UpdateSecret functionality
-	// err1 = harness.UpdateSecret("TEST_SECRET", "updatedsecret")
-	// if err1 != nil {
-	// 	fmt.Println("Failed to update secret:", err1)
-	// 	os.Exit(1)
-	// } else {
-	// 	fmt.Println("Secret updated successfully.")
-	// }
+	// Example for testing UpdateSecret functionality
+	err1 = harness.UpdateSecret("TEST_SECRET", "updatedsecret")
+	if err1 != nil {
+		fmt.Println("Failed to update secret:", err1)
+		os.Exit(1)
+	} else {
+		fmt.Println("Secret updated successfully.")
+	}
 
 	// // Example for testing DeleteSecret functionality
 	// err1 = harness.DeleteSecret("TEST_SECRET")
